7d65ac039a2bea6272694c7e8deadbb81d5f7821
ucef8233 <-u>
2020-04-17 19:30:29 +0100

    all reponces ok

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,20 @@
 const questions = document.querySelector(".questions");
+const repon = document.querySelector(".reponces");
 const suivant = document.getElementById("suivant");
 const precedent = document.getElementById("precedent");
 const demarage = document.getElementById("demarer");
 const sect = document.querySelectorAll(".sect");
-let i = 0;
-var oplalla;
-var info = [];
-//////////////////// passer de l'information au question //////////////////
 
+var oplalla;
+//////////////////// passer de l'information au question //////////////////
 var passage = function () {
   for (let i = 0; i < sect.length; i++) {
     demarage.addEventListener("click", function () {
       sect[i].classList.toggle("affiche");
-      questions.innerHTML = Question[0].question + Question[0].rep;
-      recup();
+      questions.innerHTML = Question[0];
+      repon.innerHTML = rep[1];
       precedent.classList.add("affiche");
+      blan(1);
     });
   }
   Qsuivante();
@@ -22,7 +22,7 @@ var passage = function () {
 };
 
 //////// passage de question //////
-
+let i = 0;
 var Qsuivante = function () {
   suivant.addEventListener("click", function () {
     reponces = document.querySelectorAll("#form__choice");
@@ -30,8 +30,9 @@ var Qsuivante = function () {
     if (i >= 0 && i < Question.length - 1) {
       i++;
     }
-    questions.innerHTML = Question[i].question + Question[i].rep;
-    recup();
+    questions.innerHTML = Question[i];
+    choix = document.querySelector(".questions p").className;
+    choisReponce();
   });
 };
 var Qprecedente = function () {
@@ -42,35 +43,66 @@ var Qprecedente = function () {
     if (i == 0) {
       precedent.classList.add("affiche");
     }
-    questions.innerHTML = Question[i].question + Question[i].rep;
-    recup();
+    questions.innerHTML = Question[i];
+    choix = document.querySelector(".questions p").className;
+    choisReponce();
   });
 };
-////// garder les resultat////
-var recup = function () {
-  reponces = document.querySelectorAll("#form__choice");
-  oplalla = document.getElementById("reponce1");
-  if (reponces.length === 0) {
-    oplalla.addEventListener("change", function (e) {
-      info.splice(i, 1, e.target.value);
-    });
-  } else if (reponces.length === 2) {
-    if (reponces[0].checked) {
-      info.splice(i, 1, "Oui");
-    } else if (reponces[1].checked) {
-      info.splice(i, 1, "Non");
-    }
-    for (let k = 0; k < reponces.length; k++) {
-      reponces[k].addEventListener("click", function (e) {
-        if (reponces[0].checked) {
-          info.splice(i, 1, "Oui");
-        } else if (reponces[1].checked) {
-          info.splice(i, 1, "Non");
-        }
-      });
-    }
+
+//// garder les resultat////
+
+////// metre les resultats dans un tableau qui contien '4 tableau a fin de recuperer la position cheked (i) et la position du tableau reponces.lenght
+var choisReponce = function () {
+  switch (choix) {
+    case "form__question--c":
+      repon.innerHTML = rep[0].c;
+      case0();
+      break;
+    case "form__question--age":
+      repon.innerHTML = rep[0].age;
+      case0();
+      break;
+    case "form__question--poids":
+      repon.innerHTML = rep[0].poids;
+      case0();
+      break;
+    case "form__question--taille":
+      repon.innerHTML = rep[0].taille;
+      case0();
+      break;
+    case "form__question":
+      repon.innerHTML = rep[1];
+      blan(1);
+      break;
+    case "form__question--trois":
+      repon.innerHTML = rep[2];
+      blan(2);
+      break;
+    case "form__question--quatre":
+      repon.innerHTML = rep[3];
+      blan(3);
+      break;
   }
 };
+var garderReponce = function () {};
+var blan = function (e) {
+  reponces = document.querySelectorAll("#form__choice");
+  info.splice(i, 1, tabRe[e][0]);
+  for (let k = 0; k < reponces.length; k++) {
+    reponces[k].addEventListener("click", function () {
+      if (reponces[k].checked) {
+        info.splice(i, 1, tabRe[e][k]);
+      }
+    });
+  }
+};
+var case0 = function () {
+  oplalla = document.getElementById("reponce1");
+  if (oplalla.value == "") {
+    info.splice(i, 1, tabRe[0]);
+  }
+  oplalla.addEventListener("change", function (e) {
+    info.splice(i, 1, e.target.value);
+  });
+};
 passage();
-
-// var recup = function () {};
--- src/js/general.js
+++ src/js/general.js
@@ -1,20 +1,20 @@
 const questions = document.querySelector(".questions");
+const repon = document.querySelector(".reponces");
 const suivant = document.getElementById("suivant");
 const precedent = document.getElementById("precedent");
 const demarage = document.getElementById("demarer");
 const sect = document.querySelectorAll(".sect");
-let i = 0;
-var oplalla;
-var info = [];
-//////////////////// passer de l'information au question //////////////////
 
+var oplalla;
+//////////////////// passer de l'information au question //////////////////
 var passage = function () {
   for (let i = 0; i < sect.length; i++) {
     demarage.addEventListener("click", function () {
       sect[i].classList.toggle("affiche");
-      questions.innerHTML = Question[0].question + Question[0].rep;
-      recup();
+      questions.innerHTML = Question[0];
+      repon.innerHTML = rep[1];
       precedent.classList.add("affiche");
+      blan(1);
     });
   }
   Qsuivante();
@@ -22,7 +22,7 @@ var passage = function () {
 };
 
 //////// passage de question //////
-
+let i = 0;
 var Qsuivante = function () {
   suivant.addEventListener("click", function () {
     reponces = document.querySelectorAll("#form__choice");
@@ -30,8 +30,9 @@ var Qsuivante = function () {
     if (i >= 0 && i < Question.length - 1) {
       i++;
     }
-    questions.innerHTML = Question[i].question + Question[i].rep;
-    recup();
+    questions.innerHTML = Question[i];
+    choix = document.querySelector(".questions p").className;
+    choisReponce();
   });
 };
 var Qprecedente = function () {
@@ -42,136 +43,117 @@ var Qprecedente = function () {
     if (i == 0) {
       precedent.classList.add("affiche");
     }
-    questions.innerHTML = Question[i].question + Question[i].rep;
-    recup();
+    questions.innerHTML = Question[i];
+    choix = document.querySelector(".questions p").className;
+    choisReponce();
   });
 };
-////// garder les resultat////
-var recup = function () {
-  reponces = document.querySelectorAll("#form__choice");
-  oplalla = document.getElementById("reponce1");
-  if (reponces.length === 0) {
-    oplalla.addEventListener("change", function (e) {
-      info.splice(i, 1, e.target.value);
-    });
-  } else if (reponces.length === 2) {
-    if (reponces[0].checked) {
-      info.splice(i, 1, "Oui");
-    } else if (reponces[1].checked) {
-      info.splice(i, 1, "Non");
-    }
-    for (let k = 0; k < reponces.length; k++) {
-      reponces[k].addEventListener("click", function (e) {
-        if (reponces[0].checked) {
-          info.splice(i, 1, "Oui");
-        } else if (reponces[1].checked) {
-          info.splice(i, 1, "Non");
-        }
-      });
-    }
+
+//// garder les resultat////
+
+////// metre les resultats dans un tableau qui contien '4 tableau a fin de recuperer la position cheked (i) et la position du tableau reponces.lenght
+var choisReponce = function () {
+  switch (choix) {
+    case "form__question--c":
+      repon.innerHTML = rep[0].c;
+      case0();
+      break;
+    case "form__question--age":
+      repon.innerHTML = rep[0].age;
+      case0();
+      break;
+    case "form__question--poids":
+      repon.innerHTML = rep[0].poids;
+      case0();
+      break;
+    case "form__question--taille":
+      repon.innerHTML = rep[0].taille;
+      case0();
+      break;
+    case "form__question":
+      repon.innerHTML = rep[1];
+      blan(1);
+      break;
+    case "form__question--trois":
+      repon.innerHTML = rep[2];
+      blan(2);
+      break;
+    case "form__question--quatre":
+      repon.innerHTML = rep[3];
+      blan(3);
+      break;
   }
+};
+var garderReponce = function () {};
+var blan = function (e) {
+  reponces = document.querySelectorAll("#form__choice");
+  info.splice(i, 1, tabRe[e][0]);
+  for (let k = 0; k < reponces.length; k++) {
+    reponces[k].addEventListener("click", function () {
+      if (reponces[k].checked) {
+        info.splice(i, 1, tabRe[e][k]);
+      }
+    });
+  }
+};
+var case0 = function () {
+  oplalla = document.getElementById("reponce1");
+  if (oplalla.value == "") {
+    info.splice(i, 1, tabRe[0]);
+  }
+  oplalla.addEventListener("change", function (e) {
+    info.splice(i, 1, e.target.value);
+  });
 };
 passage();
 
-// var recup = function () {};
-
-var Question = [
-  //1/////
+const Question = [
+  '<p class="form__question"> Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ? </p>',
+  '<p class="form__question--c">Quelle est votre température corporelle ?</p>',
+  '<p class="form__question"> Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?</p>',
+  '<p class="form__question"> Avez-vous eu des courbatures inhabituelles au cours des derniers jours ? </p>',
+  '<p class="form__question"> Ces derniers jours, avez-vous un mal de gorge ? </p>',
+  '<p class="form__question"> Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles. </p>',
+  '<p class="form__question">Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ? </p>',
+  '<p class="form__question">Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ? </p>',
+  '<p class="form__question"> Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ? </p>',
+  '<p class="form__question--quatre">Actuellement, comment vous vous sentez ? </p>',
+  '<p class="form__question--age">Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.</p>',
+  '<p class="form__question--poids">Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.</p>',
+  '<p class="form__question--taille">Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.</p>',
+  '<p class="form__question"> Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ? </p>',
+  '<p class="form__question">Êtes-vous diabétique ?</p>',
+  '<p class="form__question">Avez-vous ou avez-vous eu un cancer ?</p>',
+  '<p class="form__question">Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?</p>',
+  '<p class="form__question">Avez-vous une insuffisance rénale chronique dialysée ?</p>',
+  '<p class="form__question">Avez-vous une maladie chronique du foie ?</p>',
+  ' <p class="form__question--trois">Êtes-vous enceinte ?</p>',
+  '<p class="form__question">Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?</p>',
+  ' <p class="form__question">Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).</p>',
+];
+const rep = [
+  ///// 1 reponce /////
   {
-    question:
-      '<p class="form__question"> Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ? </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" value="Oui" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question" value="Non"><label for="non">Non</label><br><br></br>',
-  },
-
-  /////2 /////
-  {
-    question:
-      '<p class="form__question">Quelle est votre température corporelle ?</p>',
-    rep:
-      ///// 1 reponce /////
+    c:
       '<input type="number" id="reponce1" placeholder="34 - 42" min="34" max="42"> <label>°c</label><br><br>',
+    age:
+      '<input type="number" id="reponce1" name="question11" placeholder="15 - 110" min="15" max="110"><label>age</label><br><br></br>',
+    poids:
+      ' <input type="number" id="reponce1" name="question12" placeholder="20 - 250" min="20" max="250"><label>kg</label><br><br></br>',
+    taille:
+      '<input type="number" id="reponce1" placeholder="80 - 250" min="80" max="250"> <label>cm</label><br><br></br>',
   },
-  //////3/////
-  {
-    question:
-      '<p class="form__question"> Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?</p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-
-  /////4/////
-  {
-    question:
-      '<p class="form__question"> Avez-vous eu des courbatures inhabituelles au cours des derniers jours ? </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-  //// 5 ./////
-  {
-    question:
-      '<p class="form__question"> Ces derniers jours, avez-vous un mal de gorge ? </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-  //////6/////
-  {
-    question:
-      '<p class="form__question"> Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles. </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-  /////7//////
-  {
-    question:
-      '<p class="form__question">Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ? </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-  //////8/////
-  {
-    question:
-      '<p class="form__question">Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ? </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-  //////9/////
-  {
-    question:
-      '<p class="form__question"> Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ? </p>',
-    /// oui . non /////
-    rep:
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
-  },
-  ///////10////
-  {
-    question:
-      '<p class="form__question">Actuellement, comment vous vous sentez ? </p>',
-    rep:
-      //// 4reponce////
-      ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Bien</label><input type="radio" id="form__choice" name="question"><label for="non"> Assez bien</label><input type="radio" id="form__choice" name="question"><label for="oui">Fatigué(e) </label><input type="radio" id="form__choice" name="question"><label for="non"> Très fatigué(e)</label><br><br></br>',
-  },
-
-  // {
-  //   question:
-  //     '<p class="form__question">Actuellement, comment vous vous sentez ? </p>',
-  //   rep:
-  //     //// 4reponce////
-  //     ' <input type="radio" id="form__choice" name="question"><label for="oui">test</label><input type="radio" id="form__choice" name="question"><label for="non">test</label><input type="radio" id="form__choice" name="question"><label for="oui">test</label><input type="radio" id="form__choice" name="question"><label for="non">test</label><br><br></br>',
-  // },
-  // {
-  //   question:
-  //     '<p class="form__question">Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.</p>',
-  //   rep:
-  //     ///// 1 reponce /////
-  //     '<input type="number" name="question2" placeholder="34 - 42" min="34" max="42"> <label>°c</label><br><br>',
-  // },
+  /////////// oui non ///////////
+  ' <input type="radio" id="form__choice" name="question" checked><label for="oui">Oui</label> <br> <input type="radio" id="form__choice" name="question"><label for="non">Non</label><br><br></br>',
+  /////// 3reponces ////////////
+  ' <input type="radio" id="form__choice" name="question" checked> <label for="oui">Oui</label> <input type="radio" id="form__choice" name="question"> <label for="non">Non</label><input type="radio" id="form__choice" name="question"  ><label for="homme">Homme</label><br><br></br>',
+  /////// 4reponces////////////
+  '<input type="radio" id="form__choice" name="question" checked  ><label for="bien">Bien</label><input type="radio" id="form__choice" name="question" > <label for="abien">Assez bien</label>  <input type="radio" id="form__choice" name="question" > <label for="fatigue">Fatigué(e)</label> <input type="radio" id="form__choice" name="question" > <label for="tfatigue">Très fatigué(e)</label><br><br></br>',
+];
+var tabRe = [
+  "undefined",
+  ["Oui", "Non"],
+  ["Oui", "Non", "Homme"],
+  ["Bien", "Assez bien", "Fatigué", "Très fatigué"],
 ];
 var info = [];
